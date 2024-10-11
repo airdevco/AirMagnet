3ed392207d75f5f5e1e54763093657e7959aea0b
hadiairdev <hadi@airdev.co>
2024-10-11 20:23:37 +0700

    Create AirMagnetPopup.js

---
+++ AirMagnetPopup.js
@@ -0,0 +1,30 @@
+ocument.addEventListener('DOMContentLoaded', function() {
+    const popup = document.getElementById('AirMagnet-PopupContainer');
+    const closeBtn = document.getElementById('AirMagnet-ClosePopup');
+    const iframe = document.getElementById('AirMagnet-Iframe');
+
+    // Function to show the popup
+    window.AirMagnetShowPopup = function() {
+        popup.style.display = 'block';
+    }
+
+    // Close popup when close button is clicked
+    closeBtn.onclick = function() {
+        popup.style.display = 'none';
+    }
+
+    // Close popup when clicking outside of the popup content
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+        }
+    }
+
+    // Update iframe height dynamically after the content is fully loaded
+    window.addEventListener('message', function(event) {
+
+        const height = event.data; // Get the height from the message
+        iframe.style.height = height + 'px'; // Set the iframe height
+    });
+
+});
